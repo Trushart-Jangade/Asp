fabe75e6747a4d1c745dbf904e104d23a4a7c44d
Trushart Jangade <trushartj@kpit.com>
2019-01-07 10:32:08 +0530

    changed 4

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -27,7 +27,7 @@ namespace WebApplication1
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World! Owner Mail");
+                await context.Response.WriteAsync("Hello World! Owner approval");
             });
         }
     }
